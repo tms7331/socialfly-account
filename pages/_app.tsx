60fc108e2673286cfa6773f707e1c606295d1c15
Jordan Bettencourt <jmbettenc@gmail.com>
2024-10-10 14:19:47 -0700

    Basic encrypt/decrypt functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { mainnet, polygon, optimism, arbitrum, base, baseSepolia } from "viem/chains";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const config = getDefaultConfig({
+    appName: "LitProtocolEncrypt",
+    projectId: "YOUR_PROJECT_ID",
+    chains: [mainnet, polygon, optimism, arbitrum, base, baseSepolia],
+    ssr: true // If your dApp uses server side rendering (SSR)
+  });
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={new QueryClient()}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,145 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { useEffect, useState } from "react";
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
+import { LitNetwork } from "@lit-protocol/constants";
+import {
+  SignProtocolClient,
+  SpMode,
+  EvmChains,
+  IndexService,
+  decodeOnChainData,
+  DataLocationOnChain,
+  chainInfo,
+  SchemaItem
+} from "@ethsign/sp-sdk";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useChainId } from "wagmi";
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [message, handleMessage] = useState("");
+  const [attId, handleAttId] = useState("");
+  const [result, handleResult] = useState("");
+  const [litNodeClient, handleLitNodeClient] = useState<LitJsSdk.LitNodeClient>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  const { address } = useAccount();
+  const chainId = useChainId();
+  const schemaId = "0x300";
+
+  const connect = async () => {
+    const lnc = new LitJsSdk.LitNodeClient({
+      litNetwork: LitNetwork.DatilDev
+    });
+    handleLitNodeClient(lnc);
+    await lnc.connect();
+  };
+
+  useEffect(() => {
+    connect();
+
+    return () => {
+      if (litNodeClient) litNodeClient.disconnect();
+    };
+  }, []);
+
+  const encrypt = async (message: string) => {
+    if (!litNodeClient) {
+      return {
+        ciphertext: null,
+        dataToEncryptHash: null
+      };
+    }
+
+    // See more access control conditions here: https://developer.litprotocol.com/sdk/access-control/evm/basic-examples
+    const accessControlConditions = [
+      {
+        contractAddress: "",
+        standardContractType: "",
+        chain: "ethereum",
+        method: "eth_getBalance",
+        parameters: [":userAddress", "latest"],
+        returnValueTest: {
+          comparator: ">=",
+          value: "1000000000000" // 0.000001 ETH
+        }
+      }
+    ];
+
+    // Encrypt the message
+    const { ciphertext, dataToEncryptHash } = await LitJsSdk.encryptString(
+      {
+        accessControlConditions,
+        dataToEncrypt: message
+      },
+      litNodeClient
+    );
+
+    // Return the ciphertext and dataToEncryptHash
+    return {
+      ciphertext,
+      dataToEncryptHash
+    };
+  };
+
+  return (
+    <div>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ConnectButton />
+
+        <input
+          id="message"
+          placeholder="Message to Encrypt"
+          value={message}
+          onChange={(e) => handleMessage(e.target.value)}
+        />
+        <button
+          onClick={async () => {
+            const { ciphertext, dataToEncryptHash } = await encrypt(message);
+            if (ciphertext && dataToEncryptHash) {
+              const client = new SignProtocolClient(SpMode.OnChain, {
+                chain: EvmChains.baseSepolia
+              });
+
+              const res = await client.createAttestation({
+                schemaId: schemaId,
+                data: {
+                  ciphertext,
+                  dataToEncryptHash
+                },
+                indexingValue: address?.toLowerCase() ?? ""
+              });
+              handleResult(JSON.stringify(res));
+            }
+          }}
+        >
+          Encrypt
+        </button>
+
+        <input id="attId" placeholder="Attestation ID" value={attId} onChange={(e) => handleAttId(e.target.value)} />
+        <button
+          onClick={async () => {
+            const { ciphertext, dataToEncryptHash } = await encrypt("Message!");
+            if (ciphertext && dataToEncryptHash) {
+              const indexingClient = new IndexService("testnet");
+              const att = await indexingClient.queryAttestation(`onchain_evm_${chainId}_${attId}`);
+
+              if (att) {
+                const data = decodeOnChainData(att.data, DataLocationOnChain.ONCHAIN, att.schema.data as SchemaItem[]);
+
+                fetch("http://localhost:8080/lit", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(data)
+                })
+                  .then((res) => res.json())
+                  .then((result) => handleResult(JSON.stringify(result)));
+              }
+            }
+          }}
+        >
+          Decrypt
+        </button>
+
+        <p className="break-all">{result}</p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
